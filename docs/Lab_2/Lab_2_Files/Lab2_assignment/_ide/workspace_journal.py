f5b227aa2d3e76d8de8f3c2e1f233830ad49d217
woodenclock <luckyisland3710@gmail.com>
2026-02-20 22:36:51 +0800

    final update

--- docs/Lab_2/Lab_2_Files/Lab2_assignment/_ide/workspace_journal.py
+++ docs/Lab_2/Lab_2_Files/Lab2_assignment/_ide/workspace_journal.py
@@ -1,4 +1,4 @@
-# 2026-02-20T14:41:24.127077100
+# 2026-02-20T22:24:23.819728700
 import vitis
 
 client = vitis.create_client()
@@ -8,59 +8,5 @@ platform = client.get_component(name="platform")
 status = platform.build()
 
 comp = client.get_component(name="app_component")
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.update_hw(hw_design = "$COMPONENT_LOCATION/../../Lab2-VivadoFiles/design_1_wrapper.xsa")
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
-comp.build()
-
-status = platform.build()
-
 comp.build()
 
