146e0001374daee45d3bcedbff551c82855a923c
anneleong <anneleongsongen@gmail.com>
2026-03-03 18:56:09 +0800

    ignore files

--- docs/Lab_2/Lab_2_Files/Lab2_assignment/platform/zynqmp_fsbl/zynqmp_fsbl_bsp/libsrc/standalone/src/xarmv8_g.c
+++
@@ -1,9 +0,0 @@
-#include "xarmv8.h"
-
-XARMv8_Config XARMv8_ConfigTable[] __attribute__ ((section (".drvcfg_sec"))) = {
-	{
-		0x5f5dd19,  /* stamp-frequency */
-		0x4f790c10,  /* cpu-frequency */
-		0x0  /* reg */
-	}
-};
