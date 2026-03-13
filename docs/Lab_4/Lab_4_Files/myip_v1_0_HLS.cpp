ce815d3c62912b9c18f2cde9c1c4c5adf3676018
gavinswk <235635214+gavinswk@users.noreply.github.com>
2026-03-13 15:11:15 +0800

    stream based hls

--- docs/Lab_4/Lab_4_Files/myip_v1_0_HLS.cpp
+++ docs/Lab_4/Lab_4_Files/myip_v1_0_HLS.cpp
@@ -17,8 +17,18 @@
 #include "ap_int.h"
 #include "ap_axi_sdata.h"
 
-#define NUMBER_OF_INPUT_WORDS 4  // length of an input vector
-#define NUMBER_OF_OUTPUT_WORDS 4  // length of an input vector
+#include <stdint.h>
+
+#define A_ROWS 64
+#define A_COLS 8
+#define B_ROWS A_COLS
+#define B_COLS 1
+#define RES_ROWS A_ROWS
+#define RES_COLS B_COLS
+
+#define NUMBER_OF_INPUT_A (A_ROWS * A_COLS)
+#define NUMBER_OF_INPUT_B (B_ROWS * B_COLS) 
+#define NUMBER_OF_OUTPUT_WORDS (RES_ROWS * RES_COLS)  
 
 // ACLK, ARESETN, TREADY, TDATA, TVALID are essential signals for AXIS. New version of AXI DMA seems to expect TSTRB and/or TKEEP as well.
 
@@ -29,23 +39,46 @@ void myip_v1_0_HLS(hls::stream<AXIS>& S_AXIS, hls::stream<AXIS>& M_AXIS){
 #pragma HLS INTERFACE axis port=S_AXIS
 #pragma HLS INTERFACE axis port=M_AXIS
 
+	int A_IN[A_ROWS][A_COLS];
+	int B_IN[B_ROWS][B_COLS];
+	int RES_OUT[RES_ROWS][RES_COLS];
+
+	#pragma HLS array_partition variable=A_IN
+	#pragma HLS array_partition variable=B_IN
+	#pragma HLS array_partition variable=RES_OUT
+
 	int word_cnt;
-	ap_uint<8> sum = 0; // using arbitrary precision
+	//ap_uint<8> sum = 0; // using arbitrary precision
 	//int sum = 0;		 // using 32 bit precision
 	AXIS read_input, write_output;
 
-		myip_v1_0_HLS_for1:for(word_cnt = 0; word_cnt < NUMBER_OF_INPUT_WORDS; word_cnt++){
+		//read A matrix input
+		myip_v1_0_HLS_forA:for(word_cnt = 0; word_cnt < NUMBER_OF_INPUT_A; word_cnt++){
 			read_input = S_AXIS.read();
-			// read_input is the element (data + other signals) received by our ip through S_AXIS in one clock cycle (which contains one word).
-			// read() extracts it from the stream. Overloaded operator >> can also be used.
-			sum += read_input.data; //extracting that word
-			// We are not making using of S_AXIS_TLAST in this example.
-			// S_AXIS_TLAST is required only when we are receiving an unknown number of words.
+			A_IN[word_cnt / A_COLS][word_cnt % A_COLS] = read_input.data;
+		}
+
+		//read B matrix input
+		myip_v1_0_HLS_forB:for(word_cnt = 0; word_cnt < NUMBER_OF_INPUT_B; word_cnt++){
+			read_input = S_AXIS.read();
+			B_IN[word_cnt / B_COLS][word_cnt % B_COLS] = read_input.data;
+		}
+
+		//matrix multiplication
+		myip_v1_0_HLS_forMult:for(int i = 0; i < A_ROWS; i++){
+		#pragma HLS UNROLL
+			for (int k = 0; k < B_COLS; k++){
+				int32_t acc = 0;
+				for (int j = 0; j < A_COLS; j++){
+					acc += (int32_t)A_IN[i][j] * (int32_t)B_IN[j][k];
+				}
+			RES_OUT[i][k] = (int)(acc >> 8);
+			}
 		}
 
 		myip_v1_0_HLS_for2:for(word_cnt = 0; word_cnt < NUMBER_OF_OUTPUT_WORDS; word_cnt++){
 			//write_output.data = sum.to_int() + word_cnt;	// using arbitrary precision internally but int for interfacing
-			write_output.data = sum + word_cnt;	// using 32 bit precision or arbitrary precision all the way
+			write_output.data = RES_OUT[word_cnt / RES_COLS][word_cnt % RES_COLS];	// using 32 bit precision or arbitrary precision all the way
 			// write_output is the element sent by our ip through M_AXIS in one clock cycle.
 			write_output.last = 0;
 			write_output.keep = 0xFU;
